7a2cf29a17c8b77329a4ad955d649c7a7f7159ad
AbdoAshraf99 <eng.abdo.ashraf22@gmail.com>
2022-04-02 01:25:20 +0200

    fcommit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://Wwwdev-env.eba-tc2fphnc.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
